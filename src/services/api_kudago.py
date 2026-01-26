3d9b4240bcb25ddf39e3c08ad3530cbf6942022f
belousovsergey56 <belousovsergej56@gmail.com>
2026-01-27 01:24:42 +0300

    написал рабочий метод для запроса к апи, удалил лишнее

--- src/services/api_kudago.py
+++ src/services/api_kudago.py
@@ -1,29 +1,14 @@
-# import sys
-# import os
-# import asyncio
-# sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
+import aiohttp
 
-# import aiohttp
 from src.core.config import config
-# from src.dependencies.http_client import get_aiohttp_session
-# from fastapi import Depends
 
-def print_url():
-    URL = f"{config.url_kuda_go}/{config.api_version}"
-    print(URL)
-    
-    return URL
-# async def get_events():
-#     param = {
-#             "page": 1,
-#             "page_size": 2,
-#             "location": "spb",
-#             "fields": "title,site_url,publication_date",
-#             "text_format": "text"
-#             }
-#     async with get_aiohttp_session(f"{URL}/lists", params=param) as resp:
-#         return await resp.json()
-
-# if __name__ == "__main__":
-    # asyncio.run(get_events)
-    # print(URL)
+async def get_events(session: aiohttp.ClientSession):
+    param = {
+            "page": 1,
+            "page_size": 2,
+            "location": "spb",
+            "fields": "title,site_url,publication_date",
+            "text_format": "text"
+            }
+    async with session.get(f"{config.get_full_url()}/lists", params=param) as resp:
+        return await resp.json()
