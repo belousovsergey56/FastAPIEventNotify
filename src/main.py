71851576b88abaa44ad26d37dd3724a6ce4a5ea3
belousovsergey56 <belousovsergej56@gmail.com>
2026-01-30 00:01:21 +0300

    добавил схему в возвращаемые значения

--- src/main.py
+++ src/main.py
@@ -2,12 +2,15 @@ import aiohttp
 
 from fastapi import FastAPI, Depends
 from services.api_kudago import get_events
+from schemas.kudado_schema import SchemaEventValidator
 from dependencies.http_client import get_aiohttp_session
+from typing import Dict
 
 app = FastAPI()
 
 @app.get("/")
-async def index(session: aiohttp.ClientSession = Depends(get_aiohttp_session)):
-    return {"result": await get_events(session)}
+async def index(session: aiohttp.ClientSession = Depends(get_aiohttp_session)) -> Dict[str, SchemaEventValidator]:
+    result = await get_events(session)
+    return {"result": result}
 
     
