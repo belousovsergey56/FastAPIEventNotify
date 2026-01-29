c9427bd1955e140bcac9e0945658d6bbe1a9dabb
belousovsergey56 <belousovsergej56@gmail.com>
2026-01-30 00:00:15 +0300

    переписал get_events на async, добавил docstring и валидатор

--- src/services/api_kudago.py
+++ src/services/api_kudago.py
@@ -1,14 +1,76 @@
 import aiohttp
+import time
 
+from datetime import datetime
 from src.core.config import config
+from src.schemas.kudado_schema import SchemaEventValidator
 
-async def get_events(session: aiohttp.ClientSession):
+DATETIME_FORMAT = "%Y-%m-%d %H:%M:%S"
+
+
+def to_unixtime() -> int:
+    """Получить текущее время в unixtime формате.
+    Returns:
+        int: 1763715782 unixtime формат, целое число 
+    """
+    return int(time.mktime(datetime.now().timetuple()))
+
+
+def to_datetime(unixtime: int) -> str:
+    """Преобразовать unixtime в datetime.
+    Returns:
+        str: возвращается строка вормате "2025-11-21 12:12:25" 
+    """
+    return str(datetime.fromtimestamp(unixtime))
+
+
+async def get_events(session: aiohttp.ClientSession) -> SchemaEventValidator:
+    """Получить список мероприятий
+    Returns:
+        dict: Возвращается словарь 
+        в котором есть ключ results, это список
+        словарей с событиями
+        {
+    "count": 82677,
+    "next": "https://kudago.com/public-api/v1.2/events/?fields=age_restriction%2Cis_free&page=2",
+    "previous": null,
+    "results": [
+        "results": [
+      {
+        "dates": [
+          {
+            "start": -62135433000,
+            "end": 253370754000
+          }
+        ],
+        "description": "Камерная экскурсия, которая оставит вас наедине с мыслями ленинградцев в осаждённом городе. Прогулка построена на основе записок и ценна тем, что воспоминаниями о блокаде пережившие её люди делились неохотно. Начните экскурсию тогда, когда будете готовы.",
+        "images": [
+          {
+            "image": "https://media.kudago.com/images/event/b5/f8/b5f81286943a865cc5eac9a17e5914f2.jpg",
+            "source": {
+              "name": "spbzoo.ru",
+              "link": "http://www.spbzoo.ru/o_nas/istoriya-zooparka/poslevoennoe-vosstanovlenie/"
+            }
+          }
+        ],
+        "place": {
+          "id": 3037
+        },
+        "title": "Аудиоэкскурсия «Блокада на Петроградской стороне с Софьей Лурье»",
+        "price": ""
+      }
+    ]
+}
+    """
     param = {
             "page": 1,
-            "page_size": 2,
+            "page_size": 5,
+            "fields": "images,dates,title,place,description,price",
             "location": "spb",
-            "fields": "title,site_url,publication_date",
-            "text_format": "text"
+            "actual_since": to_unixtime(),
+            "text_format":"text"
             }
-    async with session.get(f"{config.get_full_url()}/lists", params=param) as resp:
-        return await resp.json()
+    url = config.get_full_url()
+    async with session.get(f"{url}/events", params=param) as resp:
+        raw = await resp.json()
+    return SchemaEventValidator(**raw)
