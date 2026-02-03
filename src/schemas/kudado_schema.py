0d2615afe34f1e057e898405dcff11cb79ad4014
belousovsergey56 <belousovsergej56@gmail.com>
2026-02-03 15:02:14 +0300

    добавил схемы ко всем основным функциям api_kudago

--- src/schemas/kudado_schema.py
+++ src/schemas/kudado_schema.py
@@ -1,31 +1,65 @@
 from pydantic import BaseModel
 from typing import Optional, List
 
-class SchemaDates(BaseModel):
+class DatesModel(BaseModel):
     start: int
     end: int
 
-class SchemaSource(BaseModel):
+class SourceModel(BaseModel):
     name: Optional[str] = None
     link: Optional[str] = None
 
-class SchemaImages(BaseModel):
+class ImagesModel(BaseModel):
     image: str
-    source: SchemaSource
+    source: SourceModel
 
-class SchemaPlace(BaseModel):
+class PlaceId(BaseModel):
     id: Optional[int] = None
 
-class SchemaEvent(BaseModel):
-    dates: List[SchemaDates]
-    description: str
-    images: List[SchemaImages]
-    place: Optional[SchemaPlace] = None
-    title: str
-    price: Optional[str] = None
-    
-class SchemaEventValidator(BaseModel):
+class DefaultParam(BaseModel):
     count: int
     next: Optional[str] = None
     previous: Optional[str] = None
-    results: List[SchemaEvent]
+
+class EventResults(BaseModel):
+    dates: List[DatesModel]
+    description: str
+    images: List[ImagesModel]
+    place: Optional[PlaceId] = None
+    title: str
+    price: Optional[str] = None
+
+class GetPlacesResult(PlaceId):
+    title: Optional[str] = None
+    address: Optional[str] = None
+
+class GetCollectionsResult(BaseModel):
+    title: str
+    site_url: str
+    
+class GetMovieListResult(BaseModel):
+    id: int
+    title: str
+    description: Optional[str] = None
+    images: List[ImagesModel]
+
+class GetNewsResult(BaseModel):
+    title: str
+    description: Optional[str] = None
+    images: List[ImagesModel]
+    site_url: str
+
+class SchemaGetEvents(DefaultParam):
+    results: List[EventResults]
+
+class SchemaGetPlaces(DefaultParam):
+    results: List[GetPlacesResult] = None
+    
+class SchemaGetCollections(DefaultParam):
+    results: List[GetCollectionsResult]
+
+class SchemaGetMovieList(DefaultParam):
+    results: List[GetMovieListResult]
+
+class SchemaGetNews(DefaultParam):
+    results: List[GetNewsResult]
