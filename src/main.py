c07c05bee0414b271d94e19b6eeea905308e8e8a
belousovsergey56 <belousovsergej56@gmail.com>
2026-02-03 17:57:20 +0300

    тестовые ручки

--- src/main.py
+++ src/main.py
@@ -1,16 +1,39 @@
 import aiohttp
 
 from fastapi import FastAPI, Depends
-from services.api_kudago import get_events
-from schemas.kudado_schema import SchemaEventValidator
+from services.api_kudago import get_events, get_places, get_collections, get_movie_list, get_news
+from schemas.kudado_schema import SchemaGetEvents, SchemaGetPlaces, SchemaGetCollections, SchemaGetMovieList, SchemaGetNews
 from dependencies.http_client import get_aiohttp_session
-from typing import Dict
 
 app = FastAPI()
 
-@app.get("/")
-async def index(session: aiohttp.ClientSession = Depends(get_aiohttp_session)) -> Dict[str, SchemaEventValidator]:
-    result = await get_events(session)
-    return {"result": result}
 
-    
+@app.get("/")
+def index():
+    return {"ok": "Сервер запущен"}
+
+@app.get("/events")
+async def events(session: aiohttp.ClientSession = Depends(get_aiohttp_session)) -> SchemaGetEvents:
+    result = await get_events(session)
+    return result
+
+
+@app.get("/places/{place_id}")
+async def places(place_id: int=None, session: aiohttp.ClientSession = Depends(get_aiohttp_session)) -> SchemaGetPlaces:
+    result = await get_places(session, place_id)
+    return result
+
+@app.get("/collections")
+async def collections(session: aiohttp.ClientSession = Depends(get_aiohttp_session)) -> SchemaGetCollections:
+    result = await get_collections(session)
+    return result
+
+@app.get("/movies")
+async def movie_list(session: aiohttp.ClientSession = Depends(get_aiohttp_session)) -> SchemaGetMovieList:
+    result = await get_movie_list(session)
+    return result
+
+@app.get("/news")
+async def news(session: aiohttp.ClientSession = Depends(get_aiohttp_session)) -> SchemaGetNews:
+    result = await get_news(session)
+    return result
