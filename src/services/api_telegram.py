38c3ba8eacb14d812976edf98a6d0becfafcb2e4
belousovsergey56 <belousovsergej56@gmail.com>
2026-02-10 17:40:02 +0300

    добавил функции send_message, send_image, set_webhook

--- src/services/api_telegram.py
+++ src/services/api_telegram.py
@@ -34,7 +34,7 @@ async def check_bot(session: aiohttp.ClientSession) -> CheckBotSchema:
         return CheckBotSchema(**raw)
 
 
-async def send_message(session: aiohttp.ClientSession, chat_id: str, message: str) -> dict:
+async def send_message(session: aiohttp.ClientSession, chat_id: str, message: str):
     """Отправить сообщение
     Функция отправки сообщения на ресурс - в чат бот
     Args:
@@ -48,6 +48,40 @@ async def send_message(session: aiohttp.ClientSession, chat_id: str, message: st
             "chat_id": chat_id,
             "text": message,
             }
+    async with session.post(url, json=param) as resp:
+        raw = await resp.json()
+        print(raw)
+    return raw
+
+
+async def send_image(
+    session: aiohttp.ClientSession,
+    chat_id: str,
+    image_url: str,
+    caption_text: str
+):
+    """Отправить фото с подписью
+    Args:
+        chat_id (str): идентификатор чата
+        image_url (str): урл адрес картинки
+        caption_text (str): пост в 1024 символа и короткая подпись
+    """
+    url = f"{URL}/sendPhoto"
+    param = {
+            "chat_id": chat_id,
+            "photo": image_url,
+            "caption": caption_text
+            }
+    async with session.post(url, data=param) as resp:
+        raw = await resp.json()
+    return raw
+
+
+async def set_webhook(session: aiohttp.ClientSession, https_url: str):
+    param = {
+            "url": f"{https_url}/webhook"
+            }
+    url = f"{URL}/setWebhook"
     async with session.post(url, json=param) as resp:
         raw = await resp.json()
     return raw
