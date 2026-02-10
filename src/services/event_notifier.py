17c2b67f8cb259d79e2929bab1d255feb88165ed
belousovsergey56 <belousovsergej56@gmail.com>
2026-02-11 00:08:26 +0300

    добавил модуль event_notifier для обработки данных из апи kudago и отправки в tg апи

---
+++ src/services/event_notifier.py
@@ -0,0 +1,37 @@
+from aiohttp import ClientSession
+# from src.core.config import config
+from src.services.api_kudago import collect_data
+from src.services.api_telegram import send_message, send_image
+
+
+def prepare_message(event: dict) -> str:
+    message = ""
+    for key, value in event.items():
+        if "image" == key:
+            continue
+        if len(value) == 0:
+            continue
+        if "dates" == key:
+            message += f"Дата проведения: {value}\n"
+            continue
+        message += f"{value}\n"
+    return message
+
+
+async def post_event(
+    session: ClientSession,
+    chat_id: str,
+    message: str,
+    url_image: str=None
+):
+    if url_image is None:
+        await send_message(session, chat_id, message)
+    else:
+        await send_image(session, chat_id, url_image, message)
+
+
+async def send_event_response(session: ClientSession, chat_id: str):
+    event_data = await collect_data(session)
+    for event in event_data:
+        message = prepare_message(event)
+        await post_event(session, chat_id, message, event.get("image"))
