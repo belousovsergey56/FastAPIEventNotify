e38b5826a6814eee67b63ec30c4ca6c15e3bc802
belousovsergey56 <belousovsergej56@gmail.com>
2026-02-13 00:21:38 +0300

    написал модель для талицы чатов

---
+++ src/models/chats.py
@@ -0,0 +1,11 @@
+from datetime import datetime
+from src.models.base import Base
+from sqlalchemy import func
+from sqlalchemy.orm import Mapped, mapped_column
+
+
+class Chat(Base):
+    chat_id: Mapped[int] = mapped_column(primary_key=True)
+    created_at: Mapped[datetime] = mapped_column(
+                                                server_default=func.now(),
+                                                nullable=False)
