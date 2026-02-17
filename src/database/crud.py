7f91ec7cdbb02dfdd8739eff14fe9e01537d9a02
belousovsergey56 <belousovsergej56@gmail.com>
2026-02-17 23:14:58 +0300

    изменил имя аргумента session на db_session

--- src/database/crud.py
+++ src/database/crud.py
@@ -3,27 +3,27 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select
 
 
-async def create_chat_id(session: AsyncSession, chat_id: int) -> Chat:
+async def create_chat_id(db_session: AsyncSession, chat_id: int) -> Chat:
     new_chat = Chat(chat_id=chat_id)
-    session.add(new_chat)
-    await session.commit()
-    await session.refresh(new_chat)
+    db_session.add(new_chat)
+    await db_session.commit()
+    await db_session.refresh(new_chat)
     return new_chat
 
 
-async def read_chat(session: AsyncSession, chat_id: int) -> Chat | None:
-    return await session.get(Chat, chat_id)
+async def read_chat(db_session: AsyncSession, chat_id: int) -> Chat | None:
+    return await db_session.get(Chat, chat_id)
 
 
-async def delete_chat(session: AsyncSession, chat_id: int) -> bool:
-    get_chat = await read_chat(session, chat_id)
+async def delete_chat(db_session: AsyncSession, chat_id: int) -> bool:
+    get_chat = await read_chat(db_session, chat_id)
     if not get_chat:
         return False
-    await session.delete(get_chat)
-    await session.commit()
+    await db_session.delete(get_chat)
+    await db_session.commit()
     return True
 
 
-async def get_chat_list(session: AsyncSession) -> list[Chat]:
-    chat_list = await session.execute(select(Chat))
+async def get_chat_list(db_session: AsyncSession) -> list[Chat]:
+    chat_list = await db_session.execute(select(Chat))
     return list(chat_list.scalars().all())
