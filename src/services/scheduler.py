4d8d4d6cde9dfcfd709a1bf4beffea13845d74d1
belousovsergey56 <belousovsergej56@gmail.com>
2026-02-17 23:16:27 +0300

    написал задачу которая будет работаь в фоне, имлементация экземпляра AsyncIOScheduler

---
+++ src/services/scheduler.py
@@ -0,0 +1,31 @@
+from aiohttp import ClientSession
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from src.core.config import config
+from src.database.crud import get_chat_list
+from src.database.session import async_session
+from src.services.event_notifier import prepare_message, post_event
+from src.services.api_kudago import collect_data
+
+
+async def background_notification():
+    async with ClientSession(timeout=config.get_timeout()) as session:
+        async with async_session() as db:
+            chats = await get_chat_list(db)
+
+        if not chats:
+            print("Нет чатов")
+            return
+
+        events_list = await collect_data(session)
+
+        for event in events_list:
+            message = prepare_message(event)
+            img_url = event.get("image")
+            for chat in chats:
+                chat_id = chat.chat_id
+                try:
+                    await post_event(session, chat_id, message, img_url)
+                except Exception as e:
+                    print(f"Сообщение не отправлено. Чат: {chat_id}, {e}")
+
+scheduler = AsyncIOScheduler()
