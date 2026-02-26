f485ec32de5d7d4d30f3a2af4a8db2f75c8c5108
belousovsergey56 <belousovsergej56@gmail.com>
2026-02-26 14:50:18 +0300

    добавил в lifespan finally чтобы гарантировать закрытие сессий, добавил middleware, добавил декоратор для изъятия идентификатора чата в логи

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import aiohttp
 import sys
+import uuid
 import uvicorn
 
 from contextlib import asynccontextmanager
@@ -7,6 +8,8 @@ from fastapi import FastAPI, Depends
 from fastapi import Request, HTTPException
 from fastapi.responses import JSONResponse, Response
 from src.core.config import config
+from src.core.logger_setup import setup_app_logging
+from src.core.context import chat_id_ctx_var
 from src.dependencies.http_client import get_aiohttp_session, http_client
 from src.dependencies.database import get_db
 from src.database.crud import create_chat_id, delete_chat, get_chat_list, read_chat
@@ -17,11 +20,13 @@ from src.services.scheduler import scheduler, background_notification
 from src.schemas.tg_schema import CheckBotSchema
 from src.schemas.endpoint_schema import AddToDBSchema
 from sqlalchemy.ext.asyncio import AsyncSession
-
+from src.utils.get_chat_id import inject_chat_id
 
 @asynccontextmanager
 async def lifespan(app: FastAPI) -> None:
     """Управление ресурсами"""
+    listener = setup_app_logging()
+    listener.start()
     header = {
         "User-Agent": "FastAPIEventNotifyBot/1.0",
         "Accept": "application/json",
@@ -40,16 +45,36 @@ async def lifespan(app: FastAPI) -> None:
     scheduler.add_job(background_notification, "cron", hour=12, minute=0)
     scheduler.start()
     print("Планировщик запущен\n")
-    yield
-    await http_client.session.close()
-    scheduler.shutdown()
-    print("Планировщик остановлен\n")
-
+    try:
+        yield
+    finally:
+        await http_client.session.close()
+        scheduler.shutdown()
+        print("Планировщик остановлен\n")
+        listener.stop()
 
 app = FastAPI(
     title="FastAPI Event notify",
     lifespan=lifespan
 )
+
+
+@app.middleware("http")
+async def add_request_id(request: Request, call_next):
+    # 1. Генерируем или берем ID
+    chat_id = request.headers.get("X-Request-ID", str(uuid.uuid4()))
+    
+    # 2. Устанавливаем в контекст (важно для логгера)
+    token = chat_id_ctx_var.set(chat_id)
+    try:
+        # 3. Передаем управление дальше по цепочке
+        response = await call_next(request)
+        # 4. Добавляем ID в ответ для клиента
+        response.headers["X-Request-ID"] = chat_id
+        return response
+    finally:
+        # 5. Обязательно сбрасываем контекст
+        chat_id_ctx_var.reset(token)
 
 
 @app.get("/", response_model=CheckBotSchema)
@@ -114,6 +139,7 @@ async def chat_list(db: AsyncSession = Depends(get_db)) -> list[int]:
 
 
 @app.post("/webhook")
+@inject_chat_id
 async def webhook(
     request: Request,
     session: aiohttp.ClientSession = Depends(get_aiohttp_session),
@@ -123,6 +149,7 @@ async def webhook(
     try:
         payload = await request.json()
         if "message" in payload:
+            chat_id_ctx_var.set(payload["message"]["chat"]["id"])
             chat_id = payload["message"]["chat"]["id"]
             tg_message = payload["message"]["text"]
             if tg_message == "/start":
