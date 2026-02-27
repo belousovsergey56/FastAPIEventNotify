e59525dacba6d8213b19ddfd69893537a17548df
belousovsergey56 <belousovsergej56@gmail.com>
2026-02-27 16:30:13 +0300

    добавил cli_formatter, в файл пишется info, в консоль debug, в listener добавил cli_handler и respect_handler_level, убрал неиспользуемые функции

--- src/core/logger_setup.py
+++ src/core/logger_setup.py
@@ -1,42 +1,48 @@
 import logging
 import logging.handlers
+import sys
 from pythonjsonlogger import jsonlogger
 from queue import Queue
 from src.core.config import config
-from src.core.context import chat_id_ctx_var
 
-
-class RequestIdFilter(logging.Filter):
-    def filter(self, record):
-        record.chat_id = chat_id_ctx_var.get()
-        return True
 
 def setup_app_logging():
-    rid_filter = RequestIdFilter()
-    log_format = "%(asctime)s %(levelname)s %(name)s %(chat_id)s %(message)s %(module)s %(funcName)s"
-    formatter = jsonlogger.JsonFormatter(log_format)
+    log_format = (
+        "%(asctime)s %(levelname)s %(name)s %(message)s %(module)s %(funcName)s"
+    )
+    formatter = jsonlogger.JsonFormatter(log_format, json_ensure_ascii=False)
+    cli_formatter = logging.Formatter(fmt=log_format, datefmt="%H:%M:%S")
+
     filehandler = logging.handlers.RotatingFileHandler(
         filename=config.log_file_path,
         maxBytes=config.log_max_bytes,
         backupCount=config.log_backup_count,
-        encoding="utf-8"
+        encoding="utf-8",
     )
+
     filehandler.setFormatter(formatter)
-    
+    filehandler.setLevel("INFO")
+
+    cli_handler = logging.StreamHandler(sys.stdout)
+    cli_handler.setFormatter(fmt=cli_formatter)
+    cli_handler.setLevel("DEBUG")
+
     log_queue = Queue(-1)
     queue_handler = logging.handlers.QueueHandler(log_queue)
-    listener = logging.handlers.QueueListener(log_queue, filehandler)
-
+    listener = logging.handlers.QueueListener(
+        log_queue,
+        filehandler,
+        cli_handler,
+        respect_handler_level=True,
+    )
 
     root_logger = logging.getLogger()
-    root_logger.setLevel(config.log_level)
-    root_logger.addFilter(rid_filter)
+    root_logger.setLevel(logging.DEBUG)
     root_logger.addHandler(queue_handler)
 
     for logger_name in (None, "uvicorn", "uvicorn.access", "uvicorn.error"):
         uv_logger = logging.getLogger(logger_name)
         uv_logger.handlers = []
-        uv_logger.addFilter(rid_filter)
         uv_logger.addHandler(queue_handler)
         uv_logger.propagate = False
 
